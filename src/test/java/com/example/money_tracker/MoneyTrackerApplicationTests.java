28a2dfbc92024f8769c0be8a272cc8d7940e62d6
smoon72 <sjm9219@gmail.com>
2024-10-15 18:26:28 -0400

    Initial

---
+++ src/test/java/com/example/money_tracker/MoneyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.money_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
